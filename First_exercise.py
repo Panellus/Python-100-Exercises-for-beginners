65388d8414fe547bc3e936437b77ed9597335f76
Panellus <89691597+Panellus@users.noreply.github.com>
2022-04-15 21:53:34 +0200

    Add files via upload

---
+++ First_exercise.py
@@ -0,0 +1,12 @@
+# First Exercise from the book "100 exercises for beginners"
+
+
+print("This programs purpose is to determine whether an integer is a negative number or not \n Start by entering your "
+      "number")
+
+Number = int(input())
+
+if Number < 0:
+    print("The given number is negative")
+else:
+    print("The given number is positive")
