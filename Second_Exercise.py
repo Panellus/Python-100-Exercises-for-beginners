abf859e14d6ba7fb15082842226a19e5b3b455a5
Panellus <89691597+Panellus@users.noreply.github.com>
2022-04-17 12:18:41 +0200

    Add files via upload

---
+++ Second_Exercise.py
@@ -0,0 +1,16 @@
+# Second Exercise from the book "100 exercises for beginners"
+
+print("This program calculates the sum and multiplication of two given numbers")
+
+print("\n what is the first given number?")
+y = float(input())
+
+print("\n what is the second given number?")
+x = float(input())
+
+sums = y+x
+
+multiplication = y*x
+print("\n the two numbers multiplied is: ", multiplication)
+print("\n the two numbers sum is: ", sums)
+
